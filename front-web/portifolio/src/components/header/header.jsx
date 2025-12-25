94d5ba855d5c7663fa32201e15defb140f1b9781
RichardCourtesy <richard100alves@gmail.com>
2025-12-25 17:36:47 -0300

    starting_project

---
+++ front-web/portifolio/src/components/header/header.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <header>
+      <section>
+        <img src=".//" alt="qwe" />
+      </section>
+
+      <section>
+        <a href="">Inicio</a>
+        <a href="">Sobre</a>
+        <a href="">Certificados</a>
+        <a href="">Projetos</a>
+        <a href="">Contatos</a>
+      </section>
+
+
+
+
+    </header>
+  )
+}
+
+export {Header};
---
+++ front-web/portifolio/src/routes/home/home-page.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <main>
+      <div>
+        <p>fewfqweqweqweqe</p>
+      </div>
+    </main>
+  ) 
+}
+
+export { Home };
