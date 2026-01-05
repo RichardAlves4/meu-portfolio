2d6f175d0ae6c4a3821d36fa3160bcbeeef51c68
RichardCourtesy <richard100alves@gmail.com>
2026-01-05 12:29:36 -0300

    feat
    adicionando e estilizando o conteúdo do footer.

--- front-web/portifolio/src/components/footer/footer.jsx
+++ front-web/portifolio/src/components/footer/footer.jsx
@@ -1,45 +1,53 @@
+import "./footer.css";
+
+import linkedin from "../../assets/footer-icons/linkedin.png"
+import github from "../../assets/footer-icons/github.png"
+import email from "../../assets/footer-icons/e-mail.png"
+import whatsapp from "../../assets/footer-icons/whatsapp.png"
+import instagram from "../../assets/footer-icons/instagram.png"
+
 const Footer = () => {
   return (
     <footer>
-      <section>
         <h1>Rede/Contato</h1>
 
-        <div>
-          <a href="https://www.linkedin.com/in/richardalves4/" className="">
-            <img src="" alt=""></img>
+        <section id="anchors-content">
+        <div id="media-content">
+          <a href="https://www.linkedin.com/in/richardalves4/" className="media-icon">
+              <img src={linkedin} alt=""></img>
             <p>Linkedin</p>
           </a>
 
-          <a href="https://github.com/RichardAlves4" className="">
-            <img src="" alt="" ></img>
+          <a href="https://github.com/RichardAlves4" className="media-icon">
+            <img src={github} alt="" ></img>
             <p>GitHub</p>
           </a>
 
-          <a href="" className="">
-            <img src="" alt="" href=""></img>
+          <a href="mailto:richardalvessilva41@gmail.com" className="media-icon">
+            <img src={email} alt="" href=""></img>
             <p>E-mail</p>
           </a>
 
-          <a href="https://wa.me/5511978393539" className="">
-            <img src="" alt="" href=""></img>
+          <a href="https://wa.me/5511978393539" className="media-icon">
+            <img src={whatsapp} alt="" href=""></img>
             <p>Whatsapp</p>
           </a>
 
-          <a href="https://www.instagram.com/r1chard.alves/" className="">
-            <img src="" alt="" href=""></img>
+          <a href="https://www.instagram.com/r1chard.alves/" className="media-icon">
+            <img src={instagram} alt="" href=""></img>
             <p>Instagram</p>
           </a>
         </div>
+        
+        <div id="pageanchors-content">
+          <a href="">Sobre</a>
+          <a href="">Certificados</a>
+          <a href="">Tecnologias</a>
+          <a href="">Projetos</a>
+          <a href="">Contatos</a>
+        </div>
       </section>
-
-      <section>
-        <a href="">Sobre</a>
-        <a href="">Certificados</a>
-        <a href="">Tecnologias</a>
-        <a href="">Projetos</a>
-        <a href="">Contatos</a>
       <h4>© Copyright 2025 - Richard Alves - Todos os direitos reservados.</h4>
-      </section>
     </footer>
   )
 }
