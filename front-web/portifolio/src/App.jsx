78f5e1324c998d6b5b67957916361969adbfd278
RichardCourtesy <richard100alves@gmail.com>
2026-01-04 21:11:25 -0300

    feat

--- front-web/portifolio/src/App.jsx
+++ front-web/portifolio/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 
 import { Header } from "./components/header/header";
 import { Outlet } from 'react-router-dom'
+import { Footer } from "./components/footer/footer";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header/>
       <Outlet />
+      <Footer />
     </div>
   )
 }
--- front-web/portifolio/src/components/footer/footer.jsx
+++ front-web/portifolio/src/components/footer/footer.jsx
@@ -1,7 +1,47 @@
 const Footer = () => {
-  return {
+  return (
+    <footer>
+      <section>
+        <h1>Rede/Contato</h1>
 
-  }
+        <div>
+          <a href="https://www.linkedin.com/in/richardalves4/" className="">
+            <img src="" alt=""></img>
+            <p>Linkedin</p>
+          </a>
+
+          <a href="https://github.com/RichardAlves4" className="">
+            <img src="" alt="" ></img>
+            <p>GitHub</p>
+          </a>
+
+          <a href="" className="">
+            <img src="" alt="" href=""></img>
+            <p>E-mail</p>
+          </a>
+
+          <a href="https://wa.me/5511978393539" className="">
+            <img src="" alt="" href=""></img>
+            <p>Whatsapp</p>
+          </a>
+
+          <a href="https://www.instagram.com/r1chard.alves/" className="">
+            <img src="" alt="" href=""></img>
+            <p>Instagram</p>
+          </a>
+        </div>
+      </section>
+
+      <section>
+        <a href="">Sobre</a>
+        <a href="">Certificados</a>
+        <a href="">Tecnologias</a>
+        <a href="">Projetos</a>
+        <a href="">Contatos</a>
+      <h4>© Copyright 2025 - Richard Alves - Todos os direitos reservados.</h4>
+      </section>
+    </footer>
+  )
 }
 
-export {Footer}
+export {Footer};
