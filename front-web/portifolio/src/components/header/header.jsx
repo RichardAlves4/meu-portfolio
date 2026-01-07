cb266425d795eb184937316950a238c1d05b1bc9
RichardCourtesy <richard100alves@gmail.com>
2026-01-07 17:03:17 -0300

    Revert "Merge branch 'main' into refactor"
    This reverts commit 3b520fbe63c5bffbc009d04a2a6995a263c19a52, reversing
    changes made to 1b0482f14af704840b02a08dd24d51c0a380a8fd.

--- front-web/portifolio/src/components/header/header.jsx
+++ front-web/portifolio/src/components/header/header.jsx
@@ -13,9 +13,10 @@ const Header = () => {
 
       <section id="nav-bar">
         <a href="">Sobre</a>
+        <a href="">Formação</a>
         <a href="">Certificados</a>
-        <a href="">Tecnologias</a>
         <a href="">Projetos</a>
+        <a href="">Tecnologias</a>
         <a href="">Contatos</a>
       </section>
 
@@ -25,7 +26,7 @@ const Header = () => {
         </div>
 
         <div>
-           <button /*onClick={language()}*/>BR</button> 
+           <button>BR</button> 
         </div>
       </section>
 
