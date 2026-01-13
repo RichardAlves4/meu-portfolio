c58e44b0b36e1e76f381cd52c5f3e31e09a274e8
RichardCourtesy <130692757+RichardAlves4@users.noreply.github.com>
2026-01-12 22:56:58 -0300

    feat
    adicioando conteudo aos projetos e adiconando o style ao header

--- front-web/portifolio/src/components/header/header.jsx
+++ front-web/portifolio/src/components/header/header.jsx
@@ -1,40 +1,112 @@
-import { Link } from "react-router-dom"
-import './header.css';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./header.css";
 
-import menu_icon from "../../assets/menu.png"
-import { ThemeSelect } from "../themeSelect/themeSelect"
+import { ThemeSelect } from "../themeSelect/themeSelect";
+
+import menu_icon from "../../assets/menu.png";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="container">
-      <section id="menu-icon-content">
-        <img src={menu_icon} alt=""/>
+      <section id="menu-icon-content" 
+      onClick={toggleMenu} 
+      className={menuOpen ? "open" : ""}
+      >
+        <img src={menu_icon} alt="Menu" />
       </section>
 
-      <section id="nav-bar">
-        <a href="#about-content">Sobre</a>
-        <a href="#formation-content">Formação</a>
-        <a href="#certificates-content">Certificados</a>
-        <a href="#projects-content">Projetos</a>
-        <a href="#technologies-content">Tecnologias</a>
-        <a href="#anchors-content">Contatos</a>
+      <section id="nav-bar" className={menuOpen ? "active" : ""}>
+        <ul>
+          <li>
+            <a
+              href="#about-content"
+              className="anchor-header"
+              onClick={toggleMenu}
+            >
+              Sobre
+            </a>
+          </li>
+
+          <hr />
+
+          <li>
+            <a
+              href="#formation-content"
+              className="anchor-header"
+              onClick={toggleMenu}
+            >
+              Formação
+            </a>
+          </li>
+
+          <hr />
+
+          <li>
+            <a
+              href="#certificates-content"
+              className="anchor-header"
+              onClick={toggleMenu}
+            >
+              Certificados
+            </a>
+          </li>
+
+          <hr />
+
+          <li>
+            <a
+              href="#projects-content"
+              className="anchor-header"
+              onClick={toggleMenu}
+            >
+              Projetos
+            </a>
+          </li>
+
+          <hr />
+
+          <li>
+            <a
+              href="#technologies-content"
+              className="anchor-header"
+              onClick={toggleMenu}
+            >
+              Tecnologias
+            </a>
+          </li>
+
+          <hr />
+
+          <li>
+            <a
+              href="#anchors-content"
+              className="anchor-header"
+              onClick={toggleMenu}
+            >
+              Contatos
+            </a>
+          </li>
+        </ul>
       </section>
 
       <section id="items-content">
         <div>
-          <ThemeSelect/>
+          <ThemeSelect />
         </div>
 
         <div>
-           <button>BR</button> 
+          <button>BR</button>
         </div>
       </section>
-
-
-
-
     </header>
-  )
-}
+  );
+};
 
-export {Header};
+export { Header };
